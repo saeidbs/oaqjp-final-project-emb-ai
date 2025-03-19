fcee9b9bc148c6b46e85eb3d2f403cc6c651bb1b
skewfield <saeidbahmani1377@gmail.com>
2025-03-18 23:54:40 -0400

    Done

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,50 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    # Define the URL for the sentiment analysis API
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+ 
+
+    # Create the payload with the text to be analyzed
+    myobj = { "raw_document": { "text": text_to_analyse } }
+
+    # Set the headers with the required model ID for the API
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    # Make a POST request to the API with the payload and headers
+    response = requests.post(url, json=myobj, headers=header)
+
+    # Parse the response from the API
+    formatted_response = json.loads(response.text)
+
+    # If the response status code is 200, extract the label and score from the response
+    if response.status_code == 200:
+        anger = formatted_response['emotionPredictions'][0]['emotion']['anger']
+        disgust = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+        fear = formatted_response['emotionPredictions'][0]['emotion']['fear']
+        joy = formatted_response['emotionPredictions'][0]['emotion']['joy']
+        sadness = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+        
+    # If the response status code is 500, set label and score to None
+    if response.status_code == 500 or response.status_code == 400 :
+        anger = None
+        disgust = None
+        fear = None
+        joy = None
+        sadness = None
+
+    emotion_scores = {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness
+    }
+    max_key = None
+    if response.status_code == 200:
+        max_key = max(emotion_scores, key=emotion_scores.get)
+
+    emotion_scores['dominant_emotion'] = max_key
+    # Return the label and score in a dictionary
+    return emotion_scores
---
+++ server.py
@@ -0,0 +1,45 @@
+''' Executing this function initiates the application of sentiment
+    analysis to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
+
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detection")
+
+@app.route("/emotionDetector")
+def emo_detector():
+    """Endpoint to analyze emotions from the input text."""
+    # Retrieve the text to analyze from the request arguments
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    # Pass the text to the emotion_detector function and store the response
+    response = emotion_detector(text_to_analyze)
+
+    # Extract the emotions from response
+    anger = response['anger']
+    disgust = response['disgust']
+    fear = response['fear']
+    joy = response['joy']
+    sadness = response['sadness']
+    dominant_emotion = response['dominant_emotion']
+
+    # Check if the label is None, indicating an error or invalid input
+    if dominant_emotion is None:
+        return "Invalid text! Please try again!"
+
+    response_test = (
+        f"For the given statement, the system response is 'anger': {anger}, "
+        f"'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. "
+        f"The dominant emotion is {dominant_emotion}."
+    )
+    return response_test
+
+@app.route("/")
+def render_index_page():
+    """Render the index.html page."""
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
